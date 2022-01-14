0c7cae111827fb8a9d9f031c52c744d39d54dce3
vbcc2001 <vbcc2001@gmail.com>
2022-01-15 00:55:23 +0800

    1

---
+++ zxkj/f01_index/__init__.py
@@ -0,0 +1,1 @@
+default_app_config='zxkj.f01_index.apps.Config'
---
+++ zxkj/f01_index/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.utils import timezone
+# Create your models here.
+
+class AD(models.Model):
+
+    title = models.CharField(max_length=100, verbose_name='标题')
+    photo = models.ImageField(upload_to='AD/', blank=True, verbose_name="广告图片")
+    url = models.CharField(max_length=500, verbose_name="URL链接")
+    publish_date = models.DateTimeField(max_length=20, default=timezone.now, verbose_name="发布时间")
+    views = models.PositiveIntegerField(default=1, verbose_name="浏览量")
+    sort = models.CharField(max_length=100, verbose_name='排序')
+    state = models.CharField(max_length=100, default="1",verbose_name='状态') # 默认1 有效
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "广告"
+        verbose_name_plural = "广告列表"
+        ordering = ('-sort', '-publish_date')
+
