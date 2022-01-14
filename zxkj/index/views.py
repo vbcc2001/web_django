35d4544b0b5c7d462a09fed88a179680812f5555
vbcc2001 <vbcc2001@gmail.com>
2022-01-14 22:27:47 +0800

    1

--- zxkj/index/views.py
+++ zxkj/index/views.py
@@ -9,4 +9,4 @@ from django.views.decorators.cache import cache_page
 @cache_page(1)  # 单位：秒数，这里指缓存 15 分钟
 def index(request):
     # 返回结果
-    return render(request, 'index.html')
+    return render(request, 'f02_index/f01_index.html')
