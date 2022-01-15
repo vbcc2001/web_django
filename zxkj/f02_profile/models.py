2f08303d1c2222d63b6f0c37ea486e489ccbddb1
vbcc2001 <vbcc2001@gmail.com>
2022-01-15 22:59:02 +0800

    1

--- zxkj/f02_profile/models.py
+++ zxkj/f02_profile/models.py
@@ -2,13 +2,14 @@ from django.db import models
 # 荣誉
 class Award(models.Model):
 
-    description = models.TextField(max_length=500, blank=True, null=True, verbose_name='描述')
+    title = models.TextField(max_length=500, verbose_name='标题')
+    description = models.TextField(max_length=500, verbose_name='描述')
     photo = models.ImageField(upload_to='award/', blank=True, verbose_name="图片")
-    sort = models.CharField(max_length=100, verbose_name='排序')
+    sort = models.CharField(max_length=100,  blank=True, verbose_name='排序')
     state = models.CharField(max_length=100, default="1",verbose_name='状态') # 默认1 有效
 
     def __str__(self):
-        return self.photo
+        return self.title
 
     class Meta:
         verbose_name = verbose_name_plural = '荣誉资质'
