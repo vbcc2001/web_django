15ec24a3113a5b95fe69667820dadebab58ead15
vbcc2001 <vbcc2001@gmail.com>
2022-01-16 21:11:13 +0800

    1

--- zxkj/f08_service/urls.py
+++ zxkj/f08_service/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('doc/download/', views.doc_list, name='download'),  # 资料下载页面
     path('doc/<int:id>/', views.doc, name='doc'),  # 资料下载
     path('platform/', views.platform, name='platform'),  # 人脸识别开放平台
-    path('platform/face_detect', face_detect.face_detect, name='face_detect'),  #人脸检测Demo
+    # path('platform/face_detect', face_detect.face_detect, name='face_detect'),  #人脸检测Demo
 ]
