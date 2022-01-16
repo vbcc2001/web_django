ba8377fc347fcf22381b936fe695344baff89cc5
vbcc2001 <vbcc2001@gmail.com>
2022-01-16 17:58:43 +0800

    1

--- zxkj/f08_service/urls.py
+++ zxkj/f08_service/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from . import views
+from . import views,face_detect
 
 app_name = 'service'
 urlpatterns = [
     path('doc/download/', views.doc_list, name='download'),  # 资料下载页面
     path('doc/<int:id>/', views.doc, name='doc'),  # 资料下载
-    path('facedetect/', views.facedetect, name='facedetect'),  # 本地人脸检测api
-    path('facedetect/demo', views.facedetectDemo, name='facedetectDemo'),  #网络api人脸检测
     path('platform/', views.platform, name='platform'),  # 人脸识别开放平台
+    path('facedetect/', face_detect.face_detect, name='facedetect'),  # 本地人脸检测api
+    path('facedetect/demo', face_detect.facedetectDemo, name='facedetectDemo'),  #网络api人脸检测
 ]
